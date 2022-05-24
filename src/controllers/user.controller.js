d12b8de76d7f1a672237fc205cbf7057038488b8
Miguel Cuello <luismiguelcm2015@gmail.com>
2022-05-24 09:41:33 -0500

    I just finished the user table fuctions (create, update and delete)

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,80 @@
+import { User } from "../models/userModel.js";
+
+
+export const createUser = async (req, res) => {
+    try {
+        const { names, lastNames, dni, email, birthDate, city, country } = req.body;
+
+        const newUser = await User.create({
+            names, lastNames, dni, email, birthDate, city, country, password:123
+        });
+        res.json(newUser);
+} catch  (error) {
+    return res.status(500).json({ message: error.message  });
+}
+};
+ 
+/*UPDATE USER */
+
+export const updateUser = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        const {
+            names,
+            lastNames,
+            dni,
+            email,
+            birthDate,
+            city,
+            country,
+            
+        } =
+        req.body;
+
+
+        const user = await User.findByPk(id)
+
+        user.names = names
+        user.lastNames = lastNames
+        user.dni = dni
+        user.email = email
+        user.birthDate = birthDate
+        user.city = city
+        user.country = country
+        user.password = user.password
+        await user.save()
+
+        res.json(user)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+
+}
+
+/* DELETE USER */
+
+export const deleteUser = async (req, res) => {
+    
+    try {
+        const {
+            id
+        } = req.params
+        await User.destroy({
+            where: {
+                id,
+            }
+        });
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+
+
