1fc10d9d84752db699dcb5b69cd7f7a72f3c4242
iElectro <ejose.acc@gmail.com>
2022-05-25 10:51:11 -0500

    ssl configuration

--- src/database/database.js
+++ src/database/database.js
@@ -5,8 +5,10 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
     host: process.env.DB_HOST,
     dialect: "postgres",
     dialectOptions: {
-        ssl: true,
-        rejectUnauthorized: false
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 
