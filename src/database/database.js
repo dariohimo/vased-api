4ca34279952a8c1755014a4cf6fecbc121c600d5
iElectro <ejose.acc@gmail.com>
2022-05-25 10:43:11 -0500

    ssl false

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,7 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
     host: process.env.DB_HOST,
     dialect: "postgres",
     dialecOptions: {
-        ssl: true
+        ssl: false
     }
 });
 
