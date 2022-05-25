951656eeed71ea4d15baa9b988755080a859c33a
iElectro <ejose.acc@gmail.com>
2022-05-25 10:36:43 -0500

    ssl true

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,10 @@ import Sequelize from "sequelize";
 
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "ec2-44-194-4-127.compute-1.amazonaws.com",
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    dialecOptions: {
+        ssl: true
+    }
 });
 
