67746fe2d527832a91c7fd156bd15740ddd49a47
iElectro <ejose.acc@gmail.com>
2022-05-25 10:47:06 -0500

    ssl on AGAIN

--- src/database/database.js
+++ src/database/database.js
@@ -4,8 +4,8 @@ import Sequelize from "sequelize";
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "postgres",
-    dialecOptions: {
-        ssl: false
+    dialectOptions: {
+        ssl: true
     }
 });
 
