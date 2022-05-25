9d5e9fa5d40826ef766156722e2b6a52acf6613a
iElectro <ejose.acc@gmail.com>
2022-05-25 15:06:48 -0500

    api prefix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from "express";
 import classroomRouter from "./routes/classroom.routes.js";
-import userRouter from './routes/user.routes.js';
-import taskRouter from "./routes/task.routes.js"
+import userRouter from "./routes/user.routes.js";
+import taskRouter from "./routes/task.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import utilRouter from "./routes/util.routes.js";
+import cors from "cors";
+import { Router } from "express";
 
 
+const router = Router();
 
 const app = express();
-app.use(express.json());
-app.use("/auth", authRouter);
-app.use("/classrooms", classroomRouter);
-app.use("/users", userRouter);
-app.use("/tasks", taskRouter);
-app.use("/utils", utilRouter);
 
+app.use(cors());
+app.use(express.json());
+
+
+router.use("/auth", authRouter);
+router.use("/classrooms", classroomRouter);
+router.use("/users", userRouter);
+router.use("/tasks", taskRouter);
+router.use("/utils", utilRouter);
+
+app.use("/api/v1", router);
 export default app;
--- src/routes/classroom.routes.js
+++ src/routes/classroom.routes.js
@@ -11,20 +11,79 @@ import {
 } from "../controllers/classroom.controller.js";
 
 import { auth } from "../middlewares/auth.js";
+import { authAdmin } from "../middlewares/authAdmin.js";
 
-const router = Router()
-
-
+const router = Router();
 
 // nombredelenrutador.metodo('/nombre-de-la-ruta')
-router.get("/", auth, getClassrooms)
-router.post('/create-classroom', createClassroom)
-router.put('/update-classroom/:id', updateClassroom)
-router.delete('/delete-classroom/:id', deleteClassroom)
-router.post('/add-user', addUserToClassroom)
-router.post('/add-task', addTaskToClassroom)
-router.delete('/delete-user', deleteUserFromClassroom)
-router.delete('/delete-task', deleteTaskFromClassroom)
 
+router.get("/", auth, getClassrooms);
 
-export default router   
+// create a classroom
+/**
+ * @swagger
+ * /classrooms:
+ * post:
+ * summary: Crea una nueva clase
+ * description: Crea una nueva clase
+ * tags:
+ * - Classrooms
+ * parameters:
+ * - in: body
+ *  name: body
+ * description: body
+ * required: true
+ * schema:
+ * type: object
+ * properties:
+ * capacity:
+ * type: integer
+ * name:
+ * type: string
+ * adminDescription:
+ * type: string
+ * description:
+ * required: true
+ * endsAt:
+ * type: date
+ * required: true
+ * responses:
+ * '200':
+ * description: Success
+ * '400':
+ * description: Bad Request
+ * '401':
+ * description: Unauthorized
+ * '500':
+ * description: Internal Server Error
+ * '404':
+ * description: Not Found
+ * '409':
+ * description: Conflict
+ * '422':
+ * description: Unprocessable Entity
+ * '403':
+ * description: Forbidden
+ * '401':
+ * description: Unauthorized
+ * '405':
+ * description: Method Not Allowed
+ * '408':
+ * description: Request Timeout
+ * 
+ */
+router.post("/create-classroom", authAdmin, createClassroom);
+
+router.put("/update-classroom/:id", updateClassroom);
+
+router.delete("/delete-classroom/:id", deleteClassroom);
+
+router.post("/add-user", addUserToClassroom);
+
+router.post("/add-task", addTaskToClassroom);
+
+router.delete("/delete-user", deleteUserFromClassroom);
+
+router.delete("/delete-task", deleteTaskFromClassroom);
+
+export default router;
