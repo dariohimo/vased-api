eae832010a5edbd4b90d193b967425d489df8de9
iElectro <ejose.acc@gmail.com>
2022-05-25 10:17:57 -0500

    host fixed

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "ec2-44-194-4-127.compute-1.amazonaws.com:5432",
+    host: "ec2-44-194-4-127.compute-1.amazonaws.com",
     dialect: "postgres",
 });
 
